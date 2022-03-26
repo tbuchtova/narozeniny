2cf573ba0beb66a81ef84da839509be8e3bb500d
Tereza Plecitá <plecita.terez@gmail.com>
2022-03-26 16:01:02 +0100

    formátování

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const narozeniny = dayjs('2022-08-24');
 const zprava = document.querySelector('#msg');
 
 if (dnes.isAfter(narozeniny)) {
-  zprava.innerHTML = 'Narozeniny mám zase až příští rok :(';
+  zprava.textContent = 'Narozeniny mám zase až příští rok :(';
   zprava.classList.add('narozeniny--invalid');
 } else {
-  zprava.innerHTML = 'Už brzy budu mít narozeniny!';
+  zprava.textContent = 'Už brzy budu mít narozeniny!';
   zprava.classList.add('narozeniny--valid');
 }
